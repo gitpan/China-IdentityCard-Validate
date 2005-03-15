1ffb352b1fcf1eca6900b8835b367b77df1784ec
Fayland 林 <fayland@gmail.com>
2005-03-15 02:05:16 +0000

    Import of FAYLAND/China-IdentityCard-Validate-0.02 from CPAN.
    gitpan-cpan-distribution: China-IdentityCard-Validate
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         FAYLAND/China-IdentityCard-Validate-0.02.tar.gz
    gitpan-cpan-author:       FAYLAND
    gitpan-cpan-maturity:     released

--- lib/China/IdentityCard/Validate.pm
+++ lib/China/IdentityCard/Validate.pm
@@ -3,13 +3,22 @@ package China::IdentityCard::Validate;
 use strict;
 use vars qw($VERSION @ISA @EXPORT);
 use Exporter;
-$VERSION = '0.01';
+$VERSION = '0.02';
 @ISA = qw(Exporter);
 @EXPORT = qw(validate_id);
 
 sub validate_id {
 	my $id = shift;
 	$id =~ /^\d{17}(\d|x)$/i or return 0;
+	
+	# validate the province
+	my @province = ('','','','','','','','','','','','北京','天津','河北','山西','内蒙古','','','','','','辽宁','吉林','黑龙江','','','','','','','','上海','江苏','浙江','安微','福建','江西','山东','','','','河南','湖北','湖南','广东','广西','海南','','','','重庆','四川','贵州','云南','西藏','','','','','','','陕西','甘肃','青海','宁夏','新疆','','','','','','台湾','','','','','','','','','','香港','澳门','','','','','','','','','国外');
+	my $province = substr($id, 0, 2);
+	unless($province[$province]) { return 0; }
+	
+	# validate the birthday
+	# the regex match birthday like 19491102 or 20050315, not exactly right(wrong match 19840231).
+	unless (substr($id, 6, 8) =~ /^(19|20)\d{2}((0\d)|(11|12))(([012]\d)|(3[01]))$/) { return 0; }
 	
 	my @gene = (7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
 	my @v_code = ('1','0','X','9','8','7','6','5','4','3','2');
@@ -58,7 +67,7 @@ There is a Chinese document @ L<http://www.1313s.com/f/IDCardValidate.html>. It 
 
 =head1 RETURN VALUE
 
-if it's right, return 1. otherwise, return 0/
+if it's right, return 1. otherwise, return 0.
 
 =head1 BUGS
 
@@ -68,7 +77,7 @@ feel free to report any bugs or corrections.
 
 Fayland <fayland@gmail.com>
 
-= COPYRIGHT
+=head1 COPYRIGHT
 
 Copyright (c) 2005. Fayland. All rights reserved.
 
